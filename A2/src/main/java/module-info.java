0bc1f02aaf7693b0987595a57352528989d5721d
nvas6838 <noahvass01@gmail.com>
2022-10-07 13:50:23 +1100

    work

--- A2/src/main/java/module-info.java
+++ A2/src/main/java/module-info.java
@@ -3,12 +3,6 @@ module com.example.a2 {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires eu.hansolo.tilesfx;
-
     opens com.example.a2 to javafx.fxml;
     exports com.example.a2;
 }
