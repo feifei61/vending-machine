a65decae87708602ade12658111dd6cb6490c431
nvas6838 <noahvass01@gmail.com>
2022-10-12 14:54:36 +1100

    System class

--- A2/src/main/java/com/example/a2/HelloApplication.java
+++ A2/src/main/java/com/example/a2/HelloApplication.java
@@ -18,6 +18,7 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        System system = new System(); //use this for logic
         launch();
     }
 }
