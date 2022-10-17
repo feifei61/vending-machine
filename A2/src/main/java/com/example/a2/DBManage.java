f3e591cc96d9b9a77dff94becd8bc97506fcdf78
nvas6838 <noahvass01@gmail.com>
2022-10-17 12:25:58 +1100

    functioning login system w/ ui

--- A2/src/main/java/com/example/a2/DBManage.java
+++ A2/src/main/java/com/example/a2/DBManage.java
@@ -65,7 +65,6 @@ public class DBManage {
             ResultSet result = preparedStatement.executeQuery();
 
             if(result.isClosed()){
-                System.out.println("User not found");
                 return null;
             }
 
--- A2/src/main/java/com/example/a2/view/LoginWindow.java
+++ A2/src/main/java/com/example/a2/view/LoginWindow.java
@@ -7,10 +7,14 @@ import javafx.scene.layout.Pane;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.text.Text;
 
 public class LoginWindow implements Window{
     private Pane pane;
     private Scene scene;
+    private Text text;
+    private Text username;
+    private Text password;
     private TextField captureUsername;
     private PasswordField capturePassword;
     private int width = 400;
@@ -19,19 +23,35 @@ public class LoginWindow implements Window{
     public LoginWindow() {
         pane = new Pane();
         scene = new Scene(pane, width, height);
+        text = new Text();
+        username = new Text("Username:");
+        password = new Text("Password:");
 
         //declare shapes here
+        username.setLayoutX(130);
+        username.setLayoutY(95);
+        pane.getChildren().add(username);
+
         captureUsername = new TextField();
         pane.getChildren().add(captureUsername);
 
         captureUsername.setLayoutY(100);
         captureUsername.setLayoutX(130);
 
+        password.setLayoutX(130);
+        password.setLayoutY(145);
+        pane.getChildren().add(password);
+
         capturePassword = new PasswordField();
         pane.getChildren().add(capturePassword);
 
         capturePassword.setLayoutY(150);
         capturePassword.setLayoutX(130);
+
+        text.setText("");
+        text.setLayoutY(200);
+        text.setLayoutX(130);
+        pane.getChildren().add(text);
 
         // action event
         EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
@@ -45,15 +65,16 @@ public class LoginWindow implements Window{
                 //if user exists, try and match the password
                 if(result != null){
                     if(password.equals(result)){
-                        System.out.println("User matched!");
+                        text.setText("User matched");
                     }
                     else{
-                        System.out.println("Wrong password!");
+                        text.setText("Wrong password");
                     }
                 }
                 else{//is user does not exist create it
                     DBManage.addUser(username, password, "User");
-                    System.out.printf("User added with username %s\n", username);
+                    String displayText = "User added with username " + username;
+                    text.setText(displayText);
                 }
             }
         };
